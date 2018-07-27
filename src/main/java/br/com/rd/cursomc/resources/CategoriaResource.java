ea6f696939101d91393fc5f7ae82a0a7d47f0d26
Rodhes <rodhesc@hotmail.com>
2018-07-27 19:26:21 -0300

    teste

---
+++ src/main/java/br/com/rd/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.rd.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author rodhes
+ */
+@RestController
+@RequestMapping("/url")
+public class CategoriaResource {
+    @RequestMapping(method =RequestMethod.GET )
+    public String listar(){
+        return "Rest ok";
+    }
+    
+}
