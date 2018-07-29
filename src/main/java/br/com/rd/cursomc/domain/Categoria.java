26771465c5c1a2cc76de4d45c1e7bc720a302fed
Rodhes <rodhesc@hotmail.com>
2018-07-29 18:52:23 -0300

    Tetando a primeira clase de dominio Categoria

---
+++ src/main/java/br/com/rd/cursomc/domain/Categoria.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.rd.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author rodhes
+ */
+public class Categoria implements Serializable{
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+        
+    }
+
+    public Categoria(int id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categoria other = (Categoria) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+}
--- src/main/java/br/com/rd/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/rd/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,9 @@
  */
 package br.com.rd.cursomc.resources;
 
+import br.com.rd.cursomc.domain.Categoria;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,8 +21,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CategoriaResource {
     @RequestMapping(method =RequestMethod.GET )
     
-    public String listar(){
-        return "Rest ok";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Ecritório");
+        List <Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+                
+        
+        
+        return lista;
     }
     
 }
